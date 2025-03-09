394d4cf14e42a716335251a296132f371be4cb62
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-09 07:10:29 -0400

    create a server on specific port number and respond to empty route using get()

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import express from 'express';
 
+const app = express();
+const PORT = 3300;
+
+app.get('/', (req, res) => {
+  res.send('Hello Noders');
+});
+
+app.listen(PORT, () => {
+  console.log(`Node server is running on port number: ${PORT}`);
+});
