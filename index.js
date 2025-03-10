39e32c53271d721ac06eb937654f52e2444bd488
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-10 00:59:53 -0400

    install mongoose and complete mongodb connection

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 // Load environment variable from .env file to process.env
 dotenv.config();
 
 const app = express();
-console.log(process.env);
+
 const PORT = process.env.PORT || 3003;
+const MongoDB_URL = process.env.MONGODB_URL;
 
 app.get('/', (req, res) => {
   res.send('Hello Noders');
@@ -18,7 +20,7 @@ app.get('/nehal', (req, res) => {
 });
 
 app.get('/panchal', (req, res) => {
-  res.send('Route to Panchal');
+  res.send({ lastname: 'Panchal' });
 });
 
 app.use(
@@ -27,6 +29,15 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
+
+mongoose
+  .connect(MongoDB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:');
+  });
 
 app.listen(PORT, () => {
   console.log(`Node server is running on port number: ${PORT}`);
