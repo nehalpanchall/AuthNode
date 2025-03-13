492032402933fc22185a54208b47f5318acf1db7
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-13 01:59:41 -0400

    part 4: create a new user if not exist and wrap in try/catch block

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -6,13 +6,27 @@ const userRegister = async (req, res) => {
 
   // 2. Validate data
   if (!userName || !email || !password) {
-    res.status(400).json({ message: 'Invalid user info' });
+    res.status(400).json({ message: 'invalid user info' });
   }
 
-  // 3. check user is already exist or not
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    res.status(400).json({ message: 'user already exist' });
+  try {
+    // 3. check user is already exist or not
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ message: 'user already exist' });
+    }
+
+    console.log(existingUser);
+
+    // 4. if not exist, create new user
+    const newUser = await User.create({ userName, email, password });
+    if (newUser) {
+      res.status(200).json({ message: 'user has been created successfully' });
+    }
+
+    console.log(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error in registration process' });
   }
 };
 
