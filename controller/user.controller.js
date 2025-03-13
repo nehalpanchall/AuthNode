b984d07db82b751e2e9ea6785359f6898feba665
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-13 01:40:30 -0400

    part 2: validate user data

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,7 +2,10 @@ const userRegister = async (req, res) => {
   // 1. Get the data from request body
   const { userName, email, password } = req.body;
 
-  res.status(200).json({ message: 'get data from body' });
+  // 2. Validate data
+  if (!userName || !email || !password) {
+    res.status(400).json({ message: 'Invalid user info' });
+  }
 };
 
 export { userRegister };
