cc2aded1beccaee0181fd5d641bf78daa98dc1e8
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-13 01:49:59 -0400

    part 3: check user already exist or not in database

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,5 @@
+import User from '../model/User.model.js';
+
 const userRegister = async (req, res) => {
   // 1. Get the data from request body
   const { userName, email, password } = req.body;
@@ -5,6 +7,12 @@ const userRegister = async (req, res) => {
   // 2. Validate data
   if (!userName || !email || !password) {
     res.status(400).json({ message: 'Invalid user info' });
+  }
+
+  // 3. check user is already exist or not
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400).json({ message: 'user already exist' });
   }
 };
 
