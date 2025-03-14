9034f9eb5f13bac8fb3f63638df9b880bde80ef5
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-14 01:56:47 -0400

    generate salt round and create hash using bcrypt while password modifies

--- model/User.model.js
+++ model/User.model.js
@@ -30,7 +30,15 @@ const userSchema = new mongoose.Schema(
 
 // encrypt password using bcrypt before save into the database
 userSchema.pre('save', async function (next) {
-  next();
+  try {
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(this.password, salt);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('User', userSchema);
