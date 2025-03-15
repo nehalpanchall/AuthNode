0ec108c5de36e821a5113de34c42af8d490b7997
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-15 08:44:37 -0400

    complete password encryption using bcrypt and store encrypted password in databse during registration

--- model/User.model.js
+++ model/User.model.js
@@ -34,6 +34,7 @@ userSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(this.password, salt);
+      this.password = hash;
     }
     next();
   } catch (error) {
