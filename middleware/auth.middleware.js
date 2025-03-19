4046db0127c9248b66eff08ead56de6e1dbc4825
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-19 01:54:24 -0400

    part 3: decryot jwt token using jwt verify() and extract data from JWT token

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
 export const isLoggedIn = (req, res, next) => {
   // 1. get the JWT token from cookies
   const jwtToken = req.cookies?.jwtToken;
@@ -10,6 +15,11 @@ export const isLoggedIn = (req, res, next) => {
   console.log('JWT Token found: ', jwtToken ? 'YES' : 'NO');
 
   // 3. decode JWT token
+  const decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
+
+  if (!decodedToken) {
+    return res.status(400).json({ message: 'Invalid token' });
+  }
 
   // 4. set data in request object
 
