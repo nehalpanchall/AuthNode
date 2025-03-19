f4dab02890ca6750d0871c8739b65ef946d494d2
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-19 01:47:28 -0400

    part 1: extract jwt token from cookies using optional chaining operator

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,11 @@
 export const isLoggedIn = (req, res, next) => {
   // 1. get the JWT token from cookies
+  const jwtToken = req.cookies?.jwtToken;
+
+  if (!jwtToken) {
+    return res.status(400).json({ message: 'Invalid token' });
+  }
+
   // 2. check JWT token
   // 3. decode JWT token
   // 4. set data in request object
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,12 +5,13 @@ import {
   userLogin,
   userProfile,
 } from '../controller/user.controller.js';
+import { isLoggedIn } from '../middleware/auth.middleware.js';
 
 const route = express.Router();
 
 route.post('/register', userRegister);
 route.get('/verify/:token', userVerify);
 route.post('/login', userLogin);
-route.get('/profile', userProfile);
+route.get('/profile', isLoggedIn, userProfile);
 
 export default route;
