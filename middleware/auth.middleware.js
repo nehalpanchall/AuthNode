a06c63a12d6d6c2ee85ffefa00e53e786dab0445
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-19 01:50:50 -0400

    part 2: validate received JWT token

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,12 +2,15 @@ export const isLoggedIn = (req, res, next) => {
   // 1. get the JWT token from cookies
   const jwtToken = req.cookies?.jwtToken;
 
+  // 2. check JWT token
   if (!jwtToken) {
     return res.status(400).json({ message: 'Invalid token' });
   }
 
-  // 2. check JWT token
+  console.log('JWT Token found: ', jwtToken ? 'YES' : 'NO');
+
   // 3. decode JWT token
+
   // 4. set data in request object
 
   next();
