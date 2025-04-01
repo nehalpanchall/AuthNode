0f80703a038570a8c2f1815360739fe0e72748a5
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-01 10:13:37 -0400

    decorate errors usig array() method and push into extractedErrors array via map() iteration

--- middleware/validator.middleware.js
+++ middleware/validator.middleware.js
@@ -4,6 +4,9 @@ const validation = (req, res, next) => {
   const errors = validationResult(req);
 
   if (errors.isEmpty()) return next();
+
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
 };
 
 export { validation };
