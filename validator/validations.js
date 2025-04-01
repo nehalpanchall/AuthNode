b5adca4abd1ff989c778235323f363f86706728f
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-01 09:58:35 -0400

    define the validations for user registration using body() function

---
+++ validator/validations.js
@@ -0,0 +1,42 @@
+import { body } from 'express-validator';
+
+const registrationValidator = () => {
+  return [
+    body('userName')
+      .isString()
+      .withMessage('username must be a string')
+      .trim()
+      .notEmpty()
+      .withMessage('username cannot be empty')
+      .isLength({ min: 3 })
+      .withMessage('username must be at least 3 characters long'),
+
+    body('email')
+      .trim()
+      .isEmail()
+      .withMessage('Invalid email format')
+      .normalizeEmail(),
+
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password cannot be empty')
+      .isLength({ min: 8 })
+      .withMessage('Password must be at least 8 characters long')
+      .matches(/[A-Z]/)
+      .withMessage('Password must contain at least one uppercase letter')
+      .matches(/[a-z]/)
+      .withMessage('Password must contain at least one lowercase letter')
+      .matches(/\d/)
+      .withMessage('Password must contain at least one number')
+      .matches(/[@$!%*?&]/)
+      .withMessage(
+        'Password must contain at least one special character (@, $, !, %, *, ?, &)'
+      )
+      .not()
+      .matches(/\s/)
+      .withMessage('Password cannot contain spaces'),
+  ];
+};
+
+export { registrationValidator };
