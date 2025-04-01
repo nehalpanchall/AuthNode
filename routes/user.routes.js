8ad7b6d7521ea6c8d4089d94cd897ee5172258bf
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-01 11:04:29 -0400

    create middleware chain and remove manual validation from user login controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,13 +10,16 @@ import {
 } from '../controller/user.controller.js';
 import { isLoggedIn } from '../middleware/auth.middleware.js';
 import { validation } from '../middleware/validator.middleware.js';
-import { registrationValidator } from '../validator/validations.js';
+import {
+  registrationValidator,
+  loginValidator,
+} from '../validator/validations.js';
 
 const route = express.Router();
 
 route.post('/register', registrationValidator(), validation, userRegister);
 route.get('/verify/:token', userVerify);
-route.post('/login', userLogin);
+route.post('/login', loginValidator(), validation, userLogin);
 route.get('/profile', isLoggedIn, userProfile);
 route.get('/logout', isLoggedIn, userLogout);
 route.post('/forgotpassword', forgotPassword);
