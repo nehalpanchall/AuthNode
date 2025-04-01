6e90c9db0b88658c8dab796456cc541f6606060c
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-01 10:07:39 -0400

    invoke next() flag if validation chain passes without errors

--- middleware/validator.middleware.js
+++ middleware/validator.middleware.js
@@ -2,6 +2,8 @@ import { validationResult } from 'express-validator';
 
 const validation = (req, res, next) => {
   const errors = validationResult(req);
+
+  if (errors.isEmpty()) return next();
 };
 
 export { validation };
